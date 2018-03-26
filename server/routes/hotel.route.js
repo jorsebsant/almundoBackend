b71a709fc275b9d1b7c121cb4b9bbe7901715548
Jorge Sebastian Santamaria Larrotta <jorge.santamaria@globant.com>
2018-03-25 21:03:01 -0500

    basic search endpoint

--- server/routes/hotel.route.js
+++ server/routes/hotel.route.js
@@ -1,11 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require('fs');
-const asa = require('../data-sample/data.json');
+const underscore = require("underscore");
+const response = require('../data-sample/data.json');
 class Hotel {
     static endPoint(router) {
         router.get("/api/hotel", (req, res, next) => {
-            res.json(asa);
+            res.json(response);
+        });
+        router.get("/api/hotel/search", (req, res, next) => {
+            const data = response;
+            const filterParam = req.query.query;
+            console.log(filterParam);
+            const filtered = underscore.where(data, { id: "249942" });
+            res.json(filtered);
         });
     }
 }
--- src/routes/hotel.route.ts
+++ src/routes/hotel.route.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response, Router } from "express"
-const fs = require('fs');
+const underscore = require("underscore");
 const response = require('../data-sample/data.json');
 export class Hotel {
   public static endPoint(router: Router) {
     router.get("/api/hotel", (req: Request, res: Response, next: NextFunction) => {
       res.json(response);
+    });
+
+    router.get("/api/hotel/search", (req: Request, res: Response, next: NextFunction) => {
+      const data = response;
+      const filterParam = req.query.query;
+      const filtered = underscore.where(data, {id: "249942"});
+      res.json(filtered);
     });
   }
 
